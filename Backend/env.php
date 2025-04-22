881518348e1008af545061b158743dad4ccaa778
desmond4546 <160811167+desmond4546@users.noreply.github.com>
2025-04-22 16:33:43 +0800

    Add files via upload

---
+++ Backend/env.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DB_HOST", "project-db.cdf1ngav4cix.us-east-1.rds.amazonaws.com");  // RDS endpoint
+define("DB_USER", "admin");  // Your RDS username
+define("DB_PASS", "admin123456789");  // Your RDS password
+define("DB_NAME", "GraduationEcommerce");  // Your database name
